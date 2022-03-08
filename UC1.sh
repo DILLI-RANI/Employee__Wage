983eff40bb26d3af48698925bf193cd281d7834a
P DILLI RANI <pasupulatidillirani@gmail.com>
2022-03-08 14:40:40 +0530

    employeewage:

---
+++ UC1.sh
@@ -0,0 +1,14 @@
+\UC1
+using system;
+Console.WriteLine("Welcome to employee wage computition");
+int is_present = 1;
+Random check = new Random( );
+int empCheck = check.Next(0, 2);
+if (empCheck = is_present)
+{
+	console.WriteLine("Emploee is present:");
+}
+else
+{
+	Consile.WriteLine("Employee is absent:");
+}
