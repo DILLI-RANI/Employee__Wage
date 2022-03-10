24d6ce84066b8f8f915befbf7bf5c5ce6ce5add5
P DILLI RANI <pasupulatidillirani@gmail.com>
2022-03-10 18:03:41 +0530

    Employee:

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,15 @@
+﻿//UC1
+using System;
+Console.WriteLine("Welcome to employee wage computition");
+int is_present = 1;
+Random check = new Random();
+int empCheck = check.Next(0, 2);
+if (empCheck == is_present)
+{
+	Console.WriteLine("Emploee is present:");
+}
+else
+{
+	Console.WriteLine("Employee is absent:");
+}
+
